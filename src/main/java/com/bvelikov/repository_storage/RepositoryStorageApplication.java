b16d3e7ca81f40a4d5c710a0389f5a1120701699
BozhidarVelikov <bojidarvelikov2@gmail.com>
2024-09-30 19:33:43 +0100

    Added required starter packages for Spring Boot application

---
+++ src/main/java/com/bvelikov/repository_storage/RepositoryStorageApplication.java
@@ -0,0 +1,13 @@
+package com.bvelikov.repository_storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoryStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bvelikov/repository_storage/RepositoryStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bvelikov.repository_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
